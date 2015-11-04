1ab7b0ea30ba263ce84427563b142921490f82ae
vijay <vijaykedia1987@gmail.com>
2015-11-05 01:09:52 +0530

    Addressed review comments from Udacity reviewer and fixed toast duration

--- app/src/main/java/com/kediavijay/nanodegreeportfolio/AppListAdapter.java
+++ app/src/main/java/com/kediavijay/nanodegreeportfolio/AppListAdapter.java
@@ -1,6 +1,8 @@
 package com.kediavijay.nanodegreeportfolio;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,12 +19,16 @@ import java.util.List;
 public class AppListAdapter extends ArrayAdapter<String> {
 
     private final int resource;
-    private final List<String> appNameList;
+    @NonNull private final List<String> appNameList;
 
-    public AppListAdapter(Context context, int resource, List<String> appNameList) {
+    @NonNull private final Toast toast;
+
+    @SuppressLint("ShowToast")
+    public AppListAdapter(Context context, int resource, @NonNull final List<String> appNameList) {
         super(context, resource, appNameList);
         this.resource = resource;
         this.appNameList = appNameList;
+        toast = Toast.makeText(getContext(), "temp", Toast.LENGTH_SHORT);
     }
 
     @Override
@@ -37,11 +43,15 @@ public class AppListAdapter extends ArrayAdapter<String> {
              button= (Button) convertView.getTag();
         }
         button.setText(appNameList.get(position));
+
+        // Customizing toast message
+        final String message = "This will open " + appNameList.get(position) + " app";
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String message = "This will open " + appNameList.get(position) + " app";
-                Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+                toast.setText(message);
+                toast.show();
             }
         });
         return convertView;
--- app/src/main/java/com/kediavijay/nanodegreeportfolio/MainActivity.java
+++ app/src/main/java/com/kediavijay/nanodegreeportfolio/MainActivity.java
@@ -3,8 +3,6 @@ package com.kediavijay.nanodegreeportfolio;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,27 +17,5 @@ public class MainActivity extends AppCompatActivity {
 
         final ListView listView = (ListView) findViewById(R.id.app_list_view);
         listView.setAdapter(new AppListAdapter(this, R.layout.app_list_item_main, AppNames.getAppNames()));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
